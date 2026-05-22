1e6d2d09fff059369b3ec2531d5806b40836b7f8
johney8590-svg <johney8590@gmail.com>
2026-05-22 14:20:31 +0800

    Update Vite config to use relative base path
    Set base path to relative for proper file loading.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
-  plugins: [vue()]
-}
+export default defineConfig({
+  plugins: [vue()],
+  base: './' // 👈 這行是關鍵！確保網頁是用「相對路徑」去抓檔案，才不會白畫面
+})
